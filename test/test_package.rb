283dec0b646c7f5d712e49ae01e1f1516795c86e
maul.esel <maul.esel@go4more.de>
2014-03-17 20:20:55 +0100

    remove special test for empty zip

--- test/test_package.rb
+++ test/test_package.rb
@@ -4,12 +4,6 @@ require 'ALD/package'
 class PackageTest < Test::Unit::TestCase
   BASE_PATH = "test/data/archives"
 
-  def test_empty
-    assert_raise ALD::NoDefinitionError, "Failed to recognize empty package as invalid" do
-      ALD::Package.open("#{BASE_PATH}/empty.ald")
-    end
-  end
-
   def test_no_definition
     assert_raise ALD::NoDefinitionError, "Failed to recognize package without definition as invalid" do
       ALD::Package.open("#{BASE_PATH}/no_definition.ald")
