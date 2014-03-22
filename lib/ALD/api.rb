375ab3205f295076c53f2f8072f49d73a0469444
maul.esel <maul.esel@go4more.de>
2014-03-22 15:29:45 +0100

    fix API item store
    As the symbol key was nil, only one Item was ever created.

--- lib/ALD/api.rb
+++ lib/ALD/api.rb
@@ -104,7 +104,7 @@ module ALD
         if args.first.is_a? String # GUID
           items[args.first]
         elsif args.first.is_a? Hash # used internally to avoid multiple Item instances
-          @item_store[args.first[:id]] ||= Item.new(self, args.first)
+          @item_store[args.first['id']] ||= Item.new(self, args.first)
         end
       elsif args.length == 2 # name and version
         items[*args]
