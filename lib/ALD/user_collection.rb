354c2261e9ef3036da363f6e269517c80d6f0c9a
maul.esel <maul.esel@go4more.de>
2014-03-24 17:05:17 +0100

    Implement UserCollection#entry and #entry_filter
    This enables user retrieval via UserCollection#[] and API#user.

--- lib/ALD/user_collection.rb
+++ lib/ALD/user_collection.rb
@@ -13,6 +13,18 @@ module ALD
       private
 
       def entry(hash)
+        @api.user(hash)
+      end
+
+      def entry_filter(args)
+        unless args.length == 1 && args.first.is_a?(String)
+          raise ArgumentError
+        end
+        if /^[0-9a-fA-F]{32}$/ =~ args.first
+          { id: @api.normalize_id(args.first) }
+        else
+          { name: args.first }
+        end
       end
 
       def request
